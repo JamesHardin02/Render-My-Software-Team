0a14771b9c2e8e547ae63725b600a5db888bfc59
JamesH <jwhusc7@gmail.com>
2022-04-28 16:42:05 -0400

    centered cards and added responsiveness

--- src/page-template.js
+++ src/page-template.js
@@ -1,17 +1,17 @@
 function generateManagerCard({ name, id, email, role, officeNumber }){
   return`
-    <div class="card shadow-lg rounded-3" style="width: 18rem;">
+    <div class="card shadow border rounded-3" style="width: 18rem;">
       <div class="card-body bg-primary">
         <h3 class="card-title text-white">${name}</h3>
-        <h4 class="card-subtitle mb-2 text-white mb-2">
+        <h4 class="card-subtitle text-white mb-2">
         <i class="fa-solid fa-mug-hot p-1"></i>${role}</h4>
       </div>
       <div class="card-body bg-light"> 
         <div class="container mt-4 mb-4"> 
           <ul class="list-group list-group-flush shadow">
-            <li class="list-group-item p-3 bg-white">Employee ID: ${id}</li>
-            <li class="list-group-item">Office number: ${officeNumber}</li>
-            <li class="list-group-item p-3 bg-white"><a href="mailto:${email}" class="card-link">Email: ${email}</a></li>
+            <li class="list-group-item p-3">Employee ID: ${id}</li>
+            <li class="list-group-item p-3 ">Office number: ${officeNumber}</li>
+            <li class="list-group-item p-3"><a href="mailto:${email}" class="card-link">Email: ${email}</a></li>
           </ul>
         </div>
       </div>
@@ -22,18 +22,18 @@ function generateManagerCard({ name, id, email, role, officeNumber }){
 function generateEngineerCards(engineersArr){
   return`${engineersArr.map(({ name, id, email, role, github }) => {
     return`
-    <div class="card shadow-lg rounded-3" style="width: 18rem;">
+    <div class="card shadow border rounded-3" style="width: 18rem;">
       <div class="card-body bg-primary">
         <h3 class="card-title text-white">${name}</h3>
-        <h4 class="card-subtitle mb-2 text-white mb-2">
+        <h4 class="card-subtitle text-white mb-2">
         <i class="fa-solid fa-glasses p-1"></i></i>${role}</h4>
       </div>
       <div class="card-body bg-light"> 
         <div class="container mt-4 mb-4"> 
           <ul class="list-group list-group-flush shadow">
-            <li class="list-group-item p-3 bg-white">Employee ID: ${id}</li>
-            <li class="list-group-item p-3 bg-white"><a href="https://github.com/${github}" class="card-link">GitHub: ${github}</a></li>
-            <li class="list-group-item p-3 bg-white"><a href="mailto:${email}" class="card-link">Email: ${email}</a></li>
+            <li class="list-group-item p-3">Employee ID: ${id}</li>
+            <li class="list-group-item p-3"><a href="https://github.com/${github}" class="card-link" target="_blank" >GitHub: ${github}</a></li>
+            <li class="list-group-item p-3"><a href="mailto:${email}" class="card-link">Email: ${email}</a></li>
           </ul>
         </div>
       </div>
@@ -45,18 +45,18 @@ function generateEngineerCards(engineersArr){
 function generateInternCards(internArr){
   return`${internArr.map(({ name, id, email, role, school }) => {
     return`
-    <div class="card shadow-lg rounded-3" style="width: 18rem;">
+    <div class="card shadow border rounded-3" style="width: 18rem;">
       <div class="card-body bg-primary">
         <h3 class="card-title text-white">${name}</h3>
-        <h4 class="card-subtitle mb-2 text-white mb-2">
+        <h4 class="card-subtitle text-white mb-2">
         <i class="fa-solid fa-user-graduate p-1"></i>${role}</h4>
       </div>
       <div class="card-body bg-light"> 
         <div class="container mt-4 mb-4"> 
           <ul class="list-group list-group-flush shadow">
-            <li class="list-group-item p-3 bg-white">Employee ID: ${id}</li>
-            <li class="list-group-item p-3 bg-white">School: ${school}</li>
-            <li class="list-group-item p-3 bg-white"><a href="mailto:${email}" class="card-link">Email: ${email}</a></li>
+            <li class="list-group-item p-3">Employee ID: ${id}</li>
+            <li class="list-group-item p-3">School: ${school}</li>
+            <li class="list-group-item p-3"><a href="mailto:${email}" class="card-link">Email: ${email}</a></li>
           </ul>
         </div>
       </div>
@@ -83,9 +83,11 @@ module.exports = ({ engineers, interns, manager }) => {
     <header class="bg-danger p-5">
       <h1 class="text-center text-white">My Software Team</h1>
     </header>
-    ${generateManagerCard(manager)}
-    ${generateEngineerCards(engineers)}
-    ${generateInternCards(interns)}
+    <div class="mt-5 container d-flex flex-wrap justify-content-center gap-4">
+      ${generateManagerCard(manager)}
+      ${generateEngineerCards(engineers)}
+      ${generateInternCards(interns)}
+    </div>
   </body>
 
   <footer class="">

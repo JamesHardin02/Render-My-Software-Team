3af1b960671c4c1fa5439387ebe436a1728981e1
JamesH <jwhusc7@gmail.com>
2022-04-29 14:37:29 -0400

    added comments to generate-page.js:

--- utils/generate-page.js
+++ utils/generate-page.js
@@ -6,11 +6,11 @@ const writeFile = fileContent => {
       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
       if (err) {
         reject(err);
-        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+        // return out of the function to make sure the Promise doesn't accidentally execute the resolve() function
         return;
       }
 
-      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+      // Resolve the Promise and send the successful data to the `.then()` method
       resolve({
         ok: true,
         message: 'Your Software team is rendered on index.html!'
